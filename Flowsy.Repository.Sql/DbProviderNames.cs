5776dd4399e7a9024af2cebd788de37fb35f498c
Adrian Aviles <adrian8167e@gmail.com>
2023-02-22 08:14:31 -0700

    Database provider names
    Add constants for database provider names.

---
+++ Flowsy.Repository.Sql/DbProviderNames.cs
@@ -0,0 +1,10 @@
+namespace Flowsy.Repository.Sql;
+
+public static class DbProviderNames
+{
+    public const string MsSql = "Microsoft.Data.SqlClient";
+    public const string MySql = "MySql.Data.MySqlClient";
+    public const string OracleManaged = "Oracle.ManagedDataAccess.Client";
+    public const string OracleUnmanaged = "Oracle.DataAccess.Client";
+    public const string PostgreSql = "Npgsql";
+}
