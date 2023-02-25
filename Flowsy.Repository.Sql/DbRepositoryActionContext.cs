dfbd34e156d55aaa456e9c6d069b6e8a84dc97a2
Adrian Aviles <adrian8167e@gmail.com>
2023-02-25 12:32:28 -0700

    Fix DbRepositoryActionContext constructor

--- Flowsy.Repository.Sql/DbRepositoryActionContext.cs
+++ Flowsy.Repository.Sql/DbRepositoryActionContext.cs
@@ -5,11 +5,23 @@ namespace Flowsy.Repository.Sql;
 
 public class DbRepositoryActionContext : IRepositoryActionContext
 {
-    public DbRepositoryActionContext(IRepository repository, string actionName, object? parameters, CommandType? commandType, IDbTransaction? transaction)
+    public DbRepositoryActionContext(
+        IRepository repository,
+        string actionName,
+        object? parameters,
+        CommandType? commandType, 
+        IDbTransaction? transaction
+        ) : this(repository, actionName, parameters, new Dictionary<string, object?>(), commandType, transaction)
+    {
+        
+    }
+    
+    public DbRepositoryActionContext(IRepository repository, string actionName, object? parameters, IDictionary<string, object?> details, CommandType? commandType, IDbTransaction? transaction)
     {
         Repository = repository;
         ActionName = actionName;
         Parameters = parameters;
+        Details = details;
         CommandType = commandType;
         Transaction = transaction;
     }
